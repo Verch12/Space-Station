216813f1425414a15c9ad66b380d27439503f4ac
Lenin <zikalslava@mail.ru>
2025-04-08 02:17:29 +0300

    Background & Tilemap

--- Assets/Script/AnimationManager.cs
+++ Assets/Script/AnimationManager.cs
@@ -1,7 +1,5 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class AnimationManager : MonoBehaviour
 {
@@ -11,22 +9,12 @@ public class AnimationManager : MonoBehaviour
         { KeyCode.S, new Vector3(1, 0, 0)},
         { KeyCode.D, new Vector3(1, 1, 0)},
     };
-    
-    public float speed = 5f;
-    void Start()
-    {
-        
-    }
 
     void Update()
     {
         if (Input.anyKeyDown && Direction.TryGetValue(KeyTracker.GetLastKeyPressed(), out Vector3 direction))
         {
-            //transform.localPosition = Direction[KeyTracker.GetLastKeyPressed()];
             transform.localPosition = direction;
         }
-
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
-        transform.parent.Translate(movement * speed * Time.deltaTime);
     }
 }
